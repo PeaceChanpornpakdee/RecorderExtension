ac22809ee128de50f17fb892ca8c839ae10607fe
Krit <yothinpeace@gmail.com>
2023-10-16 20:00:33 +0700

    ENH: Actual recording and download as .wav file

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ setInterval(function()
         if(targetElement1) {
             console.log("Start Recording");
             recording_status = "Recording";
+            startRecording(); 
         }
     }
 
@@ -15,7 +16,53 @@ setInterval(function()
         if(targetElement2) {
             console.log("Stop Recording");  
             recording_status = "Idle";
+            stopRecording();
         }
     }
 
 }, 500);
+
+let recordingChunks = [];
+let recording = false;
+
+function startRecording() {
+    if (!recording) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          mediaRecorder = new MediaRecorder(stream);
+          mediaRecorder.start();
+  
+          mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+              recordingChunks.push(event.data);
+            }
+          };
+  
+          mediaRecorder.onstop = () => {
+            saveRecording();
+          };
+  
+          recording = true;
+        });
+    }
+}
+  
+function stopRecording() {
+    if (recording && mediaRecorder) {
+        mediaRecorder.stop();
+        recording = false;
+    }
+}
+  
+function saveRecording() {
+    if (recordingChunks.length > 0) {
+        const blob = new Blob(recordingChunks, { type: 'audio/wav' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recorded_audio.wav';
+        a.click();
+        recordingChunks = [];
+        URL.revokeObjectURL(url);
+    }
+}
